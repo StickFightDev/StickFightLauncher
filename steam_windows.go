b3e863c2fd11ac166e79f3dc614e56e38369b02c
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2023-06-29 00:17:22 -0400

    Fallback to C:\ for Steam, and sync with latest assemblies
    - Fake return 0ms for each player while the server is still lacking ping handling, fixes missing player names
    - Rotate spawnpoints between complete player list rather than falling back to first spawnpoint for extra players
    - Remove check for disconnected players when map changes
    - Add some debug logging used to validate packet handling
    - Read player color following ClientInit packet's player stats (modified packet structure), fixes missing custom colors of all other players when joining an existing lobby
    - Try but fail to fix handler for custom LerpPlayer packet (but progress nonetheless)
    - Allow network players on client to wait for and handle packets at all times, since the method isn't followed without an available packet anyway
    - Return from connection attempt failure after logging it, before trying to start the receiver

--- steam_windows.go
+++ steam_windows.go
@@ -7,16 +7,19 @@ import (
 func (s *Steam) GetRootFolder() string {
 	steamKey, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Valve\Steam`, registry.QUERY_VALUE)
 	if err != nil {
+		//logError("Error looking for 32-bit Steam install: %v", err)
 		steamKey, err = registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\WOW6432Node\Valve\Steam`, registry.QUERY_VALUE)
 		if err != nil {
-			return ""
+			//logError("Error looking for 64-bit Steam install: %v", err)
+			return "C:\\Program Files (x86)\\Steam"
 		}
 	}
 	defer steamKey.Close()
 
 	installPath, _, err := steamKey.GetStringValue("InstallPath")
 	if err != nil {
-		return ""
+		//logError("Error reading Steam install path key: %v", err)
+		return "C:\\Program Files (x86)\\Steam"
 	}
 	return installPath
 }
